d9a7e58acf6f2a236af967a975a9f5c2e9e76b5b
chanchano <92660578+chanchano@users.noreply.github.com>
2021-10-19 09:52:00 +0800

    Update kanji.js

--- js/kanji.js
+++ js/kanji.js
@@ -2,11 +2,11 @@ filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+  if (c == "semua") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    w3RemoveClass(x[i], "semua");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "semua");
   }
 }
 
@@ -37,7 +37,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("tombol");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
